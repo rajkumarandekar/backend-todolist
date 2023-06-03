c375224753d99692abd116c5cd6ccb0f111c28b8
rajkumarandekar <rajkumarandekar1144@gmail.com>
2023-06-03 07:25:01 +0530

    new

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 // Import required modules
-const express = require("express");
+/* const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 // Create express app
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 // Connect to MongoDB
 mongoose
@@ -32,111 +26,15 @@ mongoose
     console.error("Error connecting to the database:", err);
   });
 
-// User.js
-
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-
-const User = mongoose.model("User", userSchema);
-
 // Create Todo model
 const todoSchema = new mongoose.Schema({
   text: {
     type: String,
     required: true,
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
-app.post("/api/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    // Check if the username already exists
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).send("Username already exists");
-    }
-
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // Create a new user
-    const user = new User({ username, password: hashedPassword });
-    await user.save();
-
-    res.sendStatus(201);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error registering user");
-  }
-});
-app.post("/api/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    // Check if the username exists
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.status(400).send("Invalid username or password");
-    }
-
-    // Check if the password is correct
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
-      return res.status(400).send("Invalid username or password");
-    }
-
-    // Generate and sign a JWT token
-    const token = jwt.sign({ userId: user._id }, "secretkey");
-    res.json(token);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error logging in");
-  }
-});
-
-// Authentication middleware
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, "secretkey", (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
-app.use(cors({ origin: "https://todo-d9mw.onrender.com" }));
-
-function verifyToken(req, res, next) {
-  const token = req.header("Authorization");
-  if (!token) return res.status(401).send("Access Denied");
-
-  try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
-    next();
-  } catch (error) {
-    res.status(400).send("Invalid Token");
-  }
-}
 
 // API endpoints
 app.get("/api/todos", async (req, res) => {
@@ -200,4 +98,347 @@ app.delete("/api/todos/:id", async (req, res) => {
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+*/
+// Import required modules
+/*
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Create express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://rajkumar:Raju2804@cluster0.xcmtzeg.mongodb.net/todo?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
+
+// Create User model
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Create Todo model
+const todoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+// JWT secret key
+const secretKey = "your-secret-key";
+
+// API endpoints
+app.post("/api/signup", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error creating user");
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).send("Invalid credentials");
+    }
+    const token = jwt.sign({ userId: user._id }, secretKey);
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error logging in");
+  }
+});
+
+// Middleware for authentication
+// Middleware for authentication
+const authenticateToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(403).send("Invalid token");
+    }
+    req.user = decoded; // Add decoded payload to the request
+    next();
+  });
+};
+
+app.use(authenticateToken);
+
+app.get("/api/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find({ userId: req.user.userId });
+    res.json(todos);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching todos");
+  }
+});
+
+app.get("/api/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findOne({ _id: id, userId: req.user.userId });
+    if (!todo) {
+      return res.status(404).send("Todo not found");
+    }
+    res.json(todo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching todo");
+  }
+});
+
+app.post("/api/todos", async (req, res) => {
+  try {
+    const { text } = req.body;
+    const todo = new Todo({ text, userId: req.user.userId });
+    await todo.save();
+    res.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error saving todo");
+  }
+});
+
+app.put("/api/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { text } = req.body;
+    await Todo.updateOne({ _id: id, userId: req.user.userId }, { text });
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating todo");
+  }
+});
+
+app.delete("/api/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Todo.deleteOne({ _id: id, userId: req.user.userId });
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting todo");
+  }
+});
+
+// Start the server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+*/
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://rajkumar:" +
+      encodeURIComponent("Raju2804") +
+      "@cluster0.xcmtzeg.mongodb.net/todo?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
+
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
+
+// User model
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Register a new user
+app.post("/api/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if the username is already registered
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already registered" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    // Return a success message
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    // Handle any errors
+    console.error(error.message);
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
+
+// Login
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Find the user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Compare the entered password with the hashed password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // Create a JWT token
+    const token = jwt.sign({ userId: user._id }, "your-secret-key", {
+      expiresIn: "1h",
+    });
+
+    // Return the token
+    res.status(200).json({ token });
+  } catch (error) {
+    // Handle any errors
+    res.status(500).json({ message: "Login failed" });
+  }
+});
+
+///// todos
+
+const todoSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+app.get("/api/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find({});
+    res.json(todos);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching todos");
+  }
+});
+
+app.post("/api/todos", async (req, res) => {
+  try {
+    const todo = new Todo({ text: req.body.text });
+    await todo.save();
+    res.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error saving todo");
+  }
+});
+
+app.put("/api/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Todo.findByIdAndUpdate(id, { text: req.body.text });
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating todo");
+  }
+});
+
+app.delete("/api/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Todo.findByIdAndDelete(id);
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting todo");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
